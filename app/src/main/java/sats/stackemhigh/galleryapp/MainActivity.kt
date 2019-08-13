d71ed1df9acb02889065f5e5b01c6be94937fee5
Matthew Nelson <05nelsonm@protonmail.com>
2019-08-12 20:04:31 -0400

    added content descriptions

--- app/src/main/java/sats/stackemhigh/galleryapp/MainActivity.kt
+++ app/src/main/java/sats/stackemhigh/galleryapp/MainActivity.kt
@@ -23,15 +23,25 @@ class MainActivity : AppCompatActivity() {
 
         // Add drawables to each element of the `views` array
         views[0].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.aerial_photography_aerial_shot_aerial_view_2583847))
+        views[0].contentDescription = getString(R.string.aerial_description)
         views[1].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.afterglow_beach_cliff_2555285))
+        views[1].contentDescription = getString(R.string.beach_description)
         views[2].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.alley_architecture_buildings_2526517))
+        views[2].contentDescription = getString(R.string.dark_alley_description)
         views[3].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.architectural_design_architecture_bridge_2540653))
+        views[3].contentDescription = getString(R.string.city_view_description)
         views[4].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.beautiful_breathtaking_canada_day_2526105))
+        views[4].contentDescription = getString(R.string.canada_description)
         views[5].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.bloom_blossom_flora_2567011))
+        views[5].contentDescription = getString(R.string.flower_description)
         views[6].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.close_up_colorful_colors_2529148))
+        views[6].contentDescription = getString(R.string.shoes_description)
         views[7].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.clouds_coconut_trees_daylight_2486168))
+        views[7].contentDescription = getString(R.string.palm_trees_and_sky_description)
         views[8].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.colorful_colourful_houses_2501965))
+        views[8].contentDescription = getString(R.string.home_colors_description)
         views[9].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.wallpaper_astronomy_astrophotography_2538107))
+        views[9].contentDescription = getString(R.string.space_description)
 
         val layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
 
